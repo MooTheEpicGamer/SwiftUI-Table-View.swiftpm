1b9a59b2c17e8ee81cc118b9177c159da68dc5bf
Ryan Rasavong <ryanrasavong@icloud.com>
2023-08-22 10:55:51 -0500

    Initial Commit

---
+++ ContentView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "Peter_Griffin")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
